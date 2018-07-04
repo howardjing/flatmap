27a270301cb0941a16e2832a57aec2819c06c9f1
Howard Jing <howard.jing@gmail.com>
2018-07-04 18:01:35 -0400

    add react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("HI")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Double = () =>
+  <div>YOYHOYO</div>
+
+const AddOne = () =>
+  <div>ADDDONNN</div>
+
+
+ReactDOM.render(<Double />, document.querySelector('#double'));
+ReactDOM.render(<AddOne />, document.querySelector('#add-one'));
