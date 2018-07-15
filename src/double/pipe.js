2d6a4cf73ddc062e41c9d1dd1ffbc7a2827c17f3
Howard Jing <howard.jing@gmail.com>
2018-07-15 18:39:54 -0400

    add arrow svg logic

--- src/double/pipe.js
+++ src/double/pipe.js
@@ -5,7 +5,23 @@ type Props = {
   type: "big" | "small",
 };
 
-const centerRelativeTo = (a: number, b: number): number => Math.max((b - a) / 2, 0)
+const centerRelativeTo = (self: number, relativeTo: number): number => Math.max((relativeTo - self) / 2, 0);
+const sum = (...lengths: number[]): number => lengths.reduce((partial, next) => partial + next, 0);
+
+const ArrowHead = () => (
+  <marker id="arrow" markerWidth="10" markerHeight="10" refX="3" refY="3" orient="auto" markerUnits="strokeWidth">
+    <path d="M0,0 L0,6 L6,3 z" />
+  </marker>
+);
+
+const Arrow = ({
+  width,
+}: {
+  width: number,
+}) => (
+  <line x1="0" y1="0" x2={width} y2="0" stroke="#000" strokeWidth="2" markerEnd="url(#arrow)" />
+);
+
 const Pipe = ({
   leftHeight,
   bodyWidth,
@@ -45,7 +61,7 @@ const Pipe = ({
         strokeLinecap="butt"
       />
       <rect
-        x={edgeWidth + bodyWidth}
+        x={sum(edgeWidth, bodyWidth)}
         y={rightHeightOffset}
         width={edgeWidth}
         height={rightHeight}
@@ -77,25 +93,29 @@ const Stuff = ({ type }: Props) => {
   const verticalOffset = (pipe.height.edge / 2) + 8;
   return (
     <svg>
-      <marker id="arrow" markerWidth="10" markerHeight="10" refX="3" refY="3" orient="auto" markerUnits="strokeWidth">
-        <path d="M0,0 L0,6 L6,3 z" />
-      </marker>
+      <defs>
+        <ArrowHead />
+      </defs>
       <g>
         <text x="0" y={verticalOffset} fill="black" style={{fontSize: "20px"}}>4</text>
       </g>
 
       <g transform={`translate(30 ${verticalOffset})`}>
-        <line x1="0" y1="0" x2="50" y2="0" stroke="#000" stroke-width="2" marker-end="url(#arrow)" />
-      </g>
-
-      <g>
-        <text x={pipe.width.body + 50 + (2 * pipe.width.edge) + 60} y={verticalOffset}
-          fill="black" style={{fontSize: "20px"}}>8</text>
+        <Arrow width={50} />
       </g>
 
       <g transform="translate(100 1)">
         {/* <!-- Simple rect element --> */}
         <Pipe leftHeight={40} bodyWidth={100} bodyHeight={30} rightHeight={60} />
+      </g>
+
+      <g transform={`translate(230 ${verticalOffset})`}>
+        <Arrow width={50} />
+      </g>
+
+      <g>
+        <text x={60 + pipe.width.body + 50 + (2 * pipe.width.edge) + 60} y={verticalOffset}
+          fill="black" style={{fontSize: "20px"}}>8</text>
       </g>
 
       <g transform="translate(130 80)">
